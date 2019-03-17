0649401fa2b6521f7d24f5eee038c05be9fb8ce2
Aakash Goel <goelaakash79@gmail.com>
2019-03-17 16:57:00 +0530

    initial commit

---
+++ test/appTest.js
@@ -0,0 +1,33 @@
+const assert = require('chai').assert;
+// const sayHello = require('../index').sayHello;
+// const addNumbers = require('../index').addNumbers;
+const app = require('../index');
+
+// global results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
+
+describe('App', () => {
+   describe('sayHello()', () => {
+      it('sayHello should return hello', () => {
+         assert.equal(sayHelloResult, 'hello');
+      });
+
+      it('sayHello should return type string', () => {
+         // let result = app.sayHello();
+         assert.typeOf(sayHelloResult, 'string');
+      });
+   });
+
+   describe('addNumbers()', () => {
+      it('addNumbers should be above 5', () => {
+         // let result = app.addNumbers(5,5);
+         assert.isAbove(addNumbersResult, 5);
+      });
+
+      it('addNumbers should return type number', () => {
+         assert.typeOf(addNumbersResult, 'number');
+      });
+   });
+
+});
